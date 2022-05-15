ffe89d1dfbed0c456dd63af670779625daac8491
hasstrictlyff14 <105583543+hasstrictlyff14@users.noreply.github.com>
2022-05-15 11:11:04 +0200

    Create ItemScreenReducer.ts

---
+++ ItemScreenReducer.ts
@@ -0,0 +1,38 @@
+import Immutable from 'seamless-immutable';
+import { Action } from 'redux';
+import { createReducer } from './Services/CreateReducer';
+import type { ApplyStateFunction } from './Services/CreateReducer';
+import { convertActionToName, ItemScreenReducerActions } from '../Actions';
+
+type ItemScreenReducerState = {
+    currentPage: number;
+};
+
+const INITIAL_STATE = Immutable<ItemScreenReducerState>({
+    currentPage: 0
+});
+
+const setCurrentPage = (
+    oldState: ItemScreenReducerState,
+    { currentPage }: { currentPage: number } & Action,
+    applyNewState: ApplyStateFunction<ItemScreenReducerState>
+): ItemScreenReducerState => {
+    const newState: ItemScreenReducerState = { ...oldState };
+    newState.currentPage = currentPage;
+    return applyNewState(newState);
+};
+
+const resetState = (
+    oldState: ItemScreenReducerState,
+    _: any,
+    applyNewState: ApplyStateFunction<ItemScreenReducerState>
+): ItemScreenReducerState => {
+    return applyNewState(INITIAL_STATE);
+};
+
+const itemScreen = createReducer(INITIAL_STATE, {
+    [convertActionToName(ItemScreenReducerActions.setCurrentPage)]: setCurrentPage,
+    [convertActionToName(ItemScreenReducerActions.resetState)]: resetState
+});
+
+export { itemScreen };
